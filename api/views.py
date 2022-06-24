e470fe2668c7193481c023e19e591956dc2b889b
Ayub254 <jobkarani084j@gmail.com>
2022-06-24 22:12:44 +0300

    creates profile view

--- api/views.py
+++ api/views.py
@@ -1,15 +1,47 @@
 from rest_framework.response import Response
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
 
 from .serializer import *
 from rest_framework.views import APIView
 
 # Create your views here.
-# def profile(request):
-#     profile = Profile.objects.all()
+def index(request):
 
-#     return render(request, "profile.html", {"profile": profile})
+    return render(request, "index.html")
+    
+def create_profile(request):
+    title = "Create Profile"
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+        # return HttpResponseRedirect('/')
+
+    else:
+        form = ProfileForm()
+    return render(request, 'create_profile.html', {"form": form, "title": title})
+
+
+
+def profile(request):
+    profile = Profile.objects.all()
+
+    return render(request, "profile.html", {"profile": profile})
+
+def update_profile(request, id):
+    profile = Profile.objects.all()
+    form = UpdateProfileForm(instance=profile)
+    if request.method == "POST":
+        form = UpdateProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+            return redirect('profile')
+
+    ctx = {"form": form}
+    return render(request, 'update_prof.html', ctx)
 
 class ProfileList(APIView):
 
