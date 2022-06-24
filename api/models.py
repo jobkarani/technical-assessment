6297cf14b83d4271f93259dfe06f664f9305b900
Ayub254 <jobkarani084j@gmail.com>
2022-06-24 14:32:01 +0300

    creates class profilelist

--- api/models.py
+++ api/models.py
@@ -10,3 +10,15 @@ class  Profile(models.Model):
     phone = models.CharField(max_length=12)
     grade = models.CharField(max_length=2)
     probability = models.FloatField()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.save()
+
+    def update(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
--- api/views.py
+++ api/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from .models import *
+from api import serializer
 from .serializer import *
 from rest_framework.views import APIView
 
 # Create your views here.
+class ProfileList(APIView):
+
+    def get(self, request, format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
