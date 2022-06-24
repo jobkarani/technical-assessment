7a203dad883d695ff4c8f435ea60a7ade6a1b7be
Ayub254 <jobkarani084j@gmail.com>
2022-06-24 14:58:43 +0300

    creates profile view

--- api/views.py
+++ api/views.py
@@ -1,10 +1,16 @@
 from rest_framework.response import Response
+from django.shortcuts import render
 from .models import *
 from api import serializer
 from .serializer import *
 from rest_framework.views import APIView
 
 # Create your views here.
+def profile(request):
+    profile = Profile.objects.all()
+
+    return render(request, "profile.html", {"profile": profile})
+
 class ProfileList(APIView):
 
     def get(self, request, format=None):
