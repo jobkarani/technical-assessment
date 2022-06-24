8e7d233a91c304555ddca8cce33f0c733c730847
Ayub254 <jobkarani084j@gmail.com>
2022-06-24 14:17:13 +0300

    creates model profile

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+class  Profile(models.Model):
+    dob = models.DateTimeField(null=True)
+    email = models.EmailField(max_length=256, null=True)
+    idNumber = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=12)
+    grade = models.CharField(max_length=2)
+    probability = models.FloatField()
