c370a868d998d3b2de7e3ee8925ed265972bf380
Ayub254 <jobkarani084j@gmail.com>
2022-06-27 19:55:56 +0300

    reconfigures models

--- api/models.py
+++ api/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 import datetime
 from app import *
+from django.contrib.postgres.fields import ArrayField
 
 
 DATE_INPUT_FORMATS = ['%Y-%d-%m']
 # Create your models here.
 class  Profile(models.Model):
-    dob = models.DateField(editable=DATE_INPUT_FORMATS,null=True)
-    email = models.EmailField(max_length=256, null=True)
-    idNumber = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=12)
-    grade = models.CharField(max_length=2)
-    probability = models.FloatField()
+    dob = models.DateField(editable=DATE_INPUT_FORMATS,blank=True,null=True)
+    email = models.EmailField(max_length=256,blank=True, null=True)
+    idNumber = models.CharField(max_length=100,blank=True, null=True)
+    name = models.CharField(max_length=100,blank=True, null=True)
+    phone = ArrayField(models.CharField(max_length=12,null=True,blank=True),null=True,blank=True)
+    grade = models.CharField(max_length=2,blank=True, null=True)
+    probability = models.FloatField(blank=True, null=True)
 
     def save_profile(self):
         self.save()
