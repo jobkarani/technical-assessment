bbff495617e1da9856d6d9327ea8fb446534b8f1
Ayub254 <jobkarani084j@gmail.com>
2022-06-27 22:53:49 +0300

    reconfigures models

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
+    path('createprofile/', views.create_profile, name='create_profile'),
     path('api/profiles/', views.ProfileList.as_view()),
 ]
