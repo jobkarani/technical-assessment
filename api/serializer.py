cc1c64122c1be9a538605caf9bc1f834b6601c67
Ayub254 <jobkarani084j@gmail.com>
2022-06-28 22:57:12 +0300

    final commit

--- api/serializer.py
+++ api/serializer.py
@@ -8,31 +8,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile 
         fields = ('dob', 'email', 'idNumber','name','phone','grade','probability')
 
-# class Dataset1:
-#     def __init__(self, dob, email, idNumber,name,phone):
-#         self.dob = dob
-#         self.email = email
-#         self.idNumber =idNumber
-#         self.name = name
-#         self.phone = phone
 
-# dataset1 = Dataset1(dob='2000-20-7', email='johndoe@gmail.com', idNumber='12345678', name='John Doe' , phone='254711000001')
-
-# class Dataset2:
-#     def __init__(self, dob, idNumber,name,phone):
-#         self.dob = dob
-#         self.idNumber =idNumber
-#         self.name = name
-#         self.phone = phone
-
-# dataset2 = Dataset2(dob='2000-20-7', idNumber='12345678', name='John Doe' , phone=['254711000001','254711000002'])
-
-# class Dataset3:
-#     def __init__(self, grade, probability):
-#         self.grade = grade
-#         self.probability =probability
-
-# dataset3 = Dataset3(grade='BB', probability='3.8')
 
 # class Dataset1Serializer(serializers.Serializer):
 #     dob = serializers.DateField()
